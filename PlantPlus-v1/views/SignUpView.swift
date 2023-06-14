70c3dfd666e2fc218e3cf226fa16f4387d17e7d0
Gelareh Kamel Zadeh <gelareh.kamelzadeh@outlook.com>
2023-06-14 15:54:38 -0400

    changes and validation to sign up view

--- PlantPlus-v1/views/SignUpView.swift
+++ PlantPlus-v1/views/SignUpView.swift
@@ -1,88 +1,123 @@
-//
-//  SignUpView.swift
-//  PlantPlus-v1
-//
-//  Created by Test on 2023-06-01.
-//
-
 import SwiftUI
 
 struct SignUpView: View {
     
-    @EnvironmentObject var fireAuthHelper : FireAuthHelper
+    @EnvironmentObject var fireAuthHelper: FireAuthHelper
     
-    @State private var email : String = ""
-    @State private var password : String = ""
-    @State private var confirmPassword : String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var confirmPassword: String = ""
+    @State private var name: String = ""
+    @State private var contactNumber: String = ""
     
-    @State private var name : String = ""
-    @State private var contactNumber : String = ""
+    @State private var showAlert: Bool = false
+    @State private var alertMessage: String = ""
     
     @Binding var rootScreen: RootView
     
     var body: some View {
-        VStack{
-            Form{
-                Section("Sign Up Form") {
+        VStack {
+            Form {
+                Section(header: Text("Sign Up Form").foregroundColor(.black)) {
                     TextField("Enter Email", text: self.$email)
                         .disableAutocorrection(true)
-                        .textInputAutocapitalization(.never)
-                        .textContentType(.emailAddress)
                         .keyboardType(.emailAddress)
+                        .textContentType(.emailAddress)
+                        .foregroundColor(.black)
+                        .background(RoundedRectangle(cornerRadius: 5).fill(Color(.systemGreen).opacity(0.2)))
                     
                     SecureField("Enter Password", text: self.$password)
                         .disableAutocorrection(true)
-                        .textInputAutocapitalization(.never)
                         .textContentType(.password)
-                        .keyboardType(.default)
+                        .foregroundColor(.black)
+                        .background(RoundedRectangle(cornerRadius: 5).fill(Color(.systemGreen).opacity(0.2)))
                     
                     SecureField("Enter Password Again", text: self.$confirmPassword)
                         .disableAutocorrection(true)
-                        .textInputAutocapitalization(.never)
                         .textContentType(.password)
-                        .keyboardType(.default)
+                        .foregroundColor(.black)
+                        .background(RoundedRectangle(cornerRadius: 5).fill(Color(.systemGreen).opacity(0.2)))
                     
                     TextField("Enter Name", text: self.$name)
                         .disableAutocorrection(true)
+                        .foregroundColor(.black)
+                        .background(RoundedRectangle(cornerRadius: 5).fill(Color(.systemGreen).opacity(0.2)))
                     
                     TextField("Enter Contact Number", text: self.$contactNumber)
                         .disableAutocorrection(true)
                         .keyboardType(.phonePad)
+                        .foregroundColor(.black)
+                        .background(RoundedRectangle(cornerRadius: 5).fill(Color(.systemGreen).opacity(0.2)))
                 }
-            }//Forms ends
+            }
             .navigationTitle("Welcome")
             
-            Section{
+            Section {
                 Button(action: {
-                    //validate the data
-                    //such as all the inputs aere not empty
-                    //and cgeck for password tule
-                    //and display alert accordingly
-                    
-                    //if all the data is validated
-                    self.singUpUser()
-                }){
+                    if validateForm() {
+                        signUpUser()
+                    }
+                }) {
                     Text("Create Account")
-                }//Button ends
-                .disabled(self.password != self.confirmPassword && self.email.isEmpty && self.password.isEmpty && self.confirmPassword.isEmpty)
-                
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.green)
+                        .cornerRadius(10)
+                }
+                .alert(isPresented: $showAlert) {
+                    Alert(title: Text("Error"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+                }
             }
             
-        }//VStack ends
+        }
+        .accentColor(.green)
     }
     
-    func singUpUser(){
+    private func validateForm() -> Bool {
+        if email.isEmpty || password.isEmpty || confirmPassword.isEmpty || name.isEmpty || contactNumber.isEmpty {
+            showAlert(message: "All fields are required.")
+            return false
+        }
         
+        if password != confirmPassword {
+            showAlert(message: "Passwords do not match.")
+            return false
+        }
+        
+        if !isValidEmail(email) {
+            showAlert(message: "Invalid email address.")
+            return false
+        }
+        
+        if !isValidPhoneNumber(contactNumber) {
+            showAlert(message: "Invalid phone number.")
+            return false
+        }
+        
+        return true
+    }
+    
+    private func signUpUser() {
         let newUser = UserPlants(name: self.name, email: self.email, contactNumber: self.contactNumber)
         
         self.fireAuthHelper.signUp(newUser: newUser, password: self.password)
         self.rootScreen = .Home
     }
-}
-
-/*
-struct SignUpView_Previews: PreviewProvider {
-    static var previews: some View {
-        SignUpView()
+    
+    private func showAlert(message: String) {
+        alertMessage = message
+        showAlert = true
     }
-}*/
+    
+    private func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPredicate.evaluate(with: email)
+    }
+    
+    private func isValidPhoneNumber(_ phoneNumber: String) -> Bool {
+        let phoneRegEx = "^[0-9]{10}$"
+        let phonePredicate = NSPredicate(format: "SELF MATCHES %@", phoneRegEx)
+        return phonePredicate.evaluate(with: phoneNumber)
+    }
+}
